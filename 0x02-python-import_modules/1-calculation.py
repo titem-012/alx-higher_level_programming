0467dfc53452c620af6f6f01f716ff2cfe02071a
titem-012 <titemmifta@gmail.com>
2023-09-07 19:19:01 -0700

    %

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
-if name == "main":
-    from add_0 import add
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-    a = 1
-    b = 2
+    a = 10
+    b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
