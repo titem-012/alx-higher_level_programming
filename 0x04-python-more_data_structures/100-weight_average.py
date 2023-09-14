0173a75cb90bf99b0e4b18d67fb14f435ef80667
titem-012 <titemmifta@gmail.com>
2023-09-14 14:31:51 -0700

    %

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    lf not my_list:
+    if not my_list:
         return 0
 
     num = 0
     den = 0
-
 
     for tup in my_list:
         num += tup[0] * tup[1]
