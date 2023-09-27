aaa4beec7ffd1690aa0aeb388c06e4f458569073
titem-012 <titemmifta@gmail.com>
2023-09-26 19:05:21 -0700

    %

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,7 @@
 class Square:
     """Represent a square."""
 
-    def__init__(self, size=0, position=(0, 0)):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
 
         Args:
