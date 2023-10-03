474fec76e6c1f71a1f3992e8dbaab6e936852769
titem-012 <titemmifta@gmail.com>
2023-10-02 19:47:41 -0700

    %

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -125,7 +125,7 @@ def recursive_solve(board, row, queens, solutions):
     return (solutions)
 
 
-if name == "main":
+if __name__== "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
